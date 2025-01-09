43f4db9dd6d4c59f5a59659fc8a2ed59e16fc27f
Nicolas Becharat <nicolas.becharat@gmail.com>
2025-01-09 12:48:39 +0100

    chore: getUser by email & id

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -11,28 +11,33 @@ const prisma = new PrismaClient();
 export default class AuthController {
 
     // Méthode de création d'utilisateurs
-    static async register({email, hash, name }: RegisterUser) {
-        // Si il manque un élément -> renvoi une erreur
-        if (!name || !hash || !email) {
-            throw new Error("Missing required fields");
-        }
-
-        // Création d'un utilisateur
-        const user = await prisma.user.create({
-            data: {
-                email,
-                password: hash,
-                name
+    static async register({ email, hash, name }: RegisterUser) {
+        try {
+            // Si il manque un élément -> renvoi une erreur
+            if (!name || !hash || !email) {
+                throw new Error("Missing required fields");
             }
-        });
 
-        return user;
+            // Création d'un utilisateur
+            const user = await prisma.user.create({
+                data: {
+                    email,
+                    password: hash,
+                    name
+                }
+            });
+
+            return user;
+        } catch (error) {
+            // Création d'un message d'erreur
+            throw new Error("Register failed");
+        }
     }
 
     // Récupération de l'utilisateur
-    static async verifyToken(token : string) {
+    static async verifyToken(token: string) {
         try {
-            // Vérification de l'existance du TOKEN
+            // Vérification de l'existance du SECRET
             if (!process.env.JWT_SECRET) {
                 throw new Error("JWT_SECRET is not defined");
             }
@@ -45,22 +50,69 @@ export default class AuthController {
     }
 
     // Méthode de connexion
-    static async login({email, password}: LoginUser) {
+    static async login({ email, password }: LoginUser) {
         // Trouve l'utilisateur selon son email
-        const user = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        });
 
-        // Vérification de l'existance de l'utilisateur
-        if (!user) {
-            return null;
+        try {
+
+            const user = await AuthController.getUserByEmail(email);
+            // Vérification de l'existence de l'utilisateur
+            if (!user) {
+                throw new Error("User not found");
+            }
+            // Vérification du hachement du mot de passe
+            if (!CypherController.comparePassword({ password, hash: user.password })) {
+                throw new Error("Incorrect password");
+            }
+            
+            // Vérification de l'existance du SECRET
+            if (!process.env.JWT_SECRET) {
+                throw new Error("JWT_SECRET is not defined");
+            }
+
+            // Création d'un token
+            const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+            return { token, user };
+        } catch (error) {
+            // Création d'un message d'erreur
+            throw new Error("Login failed");
         }
-        // Vérification du hachement du mot de passe
-        if (!CypherController.comparePassword({ password, hash: user.password })) {
-            return null;
+    }
+
+    // Méthode de récupération d'un utilisateur
+    static async getUser(id: string) {
+        try {
+            // Vérification de l'existance de l'utilisateur
+            if (!id) {
+                throw new Error("Missing required fields");
+            }
+            const user = await prisma.user.findUnique({
+                where: {
+                    id
+                }
+            });
+            return user;
+        } catch (error) {
+            // Création d'un message d'erreur
+            throw new Error("Get user failed");
         }
-        return user;
+    }
+
+    static async getUserByEmail(email: string) {
+        try {
+            // Vérification de l'existance de l'utilisateur
+            if (!email) {
+                throw new Error("Missing required fields");
+            }
+            const user = await prisma.user.findUnique({
+                where: {
+                    email
+                }
+            });
+            return user;
+        } catch (error) {
+            // Création d'un message d'erreur
+            throw new Error("Get user failed");
+        }
     }
 }
